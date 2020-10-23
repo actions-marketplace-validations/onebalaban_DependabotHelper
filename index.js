6fa56f93ce9bb6a37ba29117ea177ef992f2d12a
Roman Balaban <romanbalaban@outlook.com>
2020-10-23 17:31:46 +0300

    change ver

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+async function run() {
+    try {
+        const token = core.getInput("github-token", { required: true });
+
+        const { pull_request: pr } = github.context.payload;
+        if (!pr) {
+            throw new Error("Event payload missing `pull_request`");
+        }
+
+        const client = new github.GitHub(token);
+        core.debug(`Creating approving review for pull request #${pr.number}`);
+        await client.pulls.createReview({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: pr.number,
+            event: "APPROVE"
+        });
+        core.debug(`Approved pull request #${pr.number}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
